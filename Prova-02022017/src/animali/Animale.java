6ad13749e45f8e11b8df75ba805e928b3abef142
gabriele <gabriele@192.168.1.100>
2017-07-25 15:26:41 +0200

    ciao

---
+++ Prova-02022017/src/animali/Animale.java
@@ -0,0 +1,95 @@
+package animali;
+
+public abstract class Animale {
+
+	private String nome;
+	private int matricola;
+	private static int matricolaProgressiva = 0;
+	private String dataNascita;
+	private int numeroZampe;
+	@SuppressWarnings("unused")
+	private String verso;
+
+	/**
+	 * Metodo costruttore
+	 * 
+	 * @param nome
+	 * @param dataNascita
+	 * @param numeroZampe
+	 * @param verso
+	 */
+	public Animale(String nome, String dataNascita, int numeroZampe,String verso) {
+		this.setNome(nome);
+		this.setMatricola(++matricolaProgressiva);
+		this.setDataNascita(dataNascita);
+		this.setNumeroZampe(numeroZampe);
+		this.setVerso(verso);
+	}
+
+	/**
+	 * Restituisce il nome dell'animale
+	 * @return
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * Imposta il nome dell'animale
+	 * @param nome
+	 */
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * Imposta la matricola dell'animale
+	 * @param matricola
+	 */
+	private void setMatricola(int matricola) {
+		this.matricola = matricola;
+	}
+
+	/**
+	 * Imposta la data di nascita dell'animale
+	 * @param dataNascita
+	 */
+	private void setDataNascita(String dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	/**
+	 * Imposta il numero di zampe dell'animale
+	 * @param numeroZampe
+	 */
+	private void setNumeroZampe(int numeroZampe) {
+		this.numeroZampe = numeroZampe;
+	}
+	
+	/**
+	 * Imposta il verso dell'animale
+	 * @param verso
+	 */
+	private void setVerso(String verso){
+		this.verso = verso;
+	}
+	
+	/**
+	 * Restituisce le informazioni sotto forma di stringa
+	 */
+	public String toString(){
+		return "Nome: "+nome+" Matricola: "+matricola+" Data di nascita:"+dataNascita+" Numero di zampe: "+numeroZampe;
+	}
+
+	@Override
+	/**
+	 * Genera l'Hashcode
+	 */
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + matricola;
+		return result;
+	}
+
+}
---
+++ Prova-02022017/src/zoo/Zoo.java
@@ -0,0 +1,194 @@
+package zoo;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeMap;
+import animali.Animale;
+import animali.Cane;
+import animali.Gatto;
+import eccezioni.AnimaleNonPresenteEccezione;
+import eccezioni.AnimalePresenteEccezione;
+import eccezioni.ZonaNonVuotaEccezione;
+import eccezioni.ZonaPresenteEccezione;
+
+public class Zoo implements Iterable<Zona>{
+
+	@SuppressWarnings("unused")
+	private String nome;
+	@SuppressWarnings("unused")
+	private String indirizzo;
+	@SuppressWarnings("unused")
+	private String città;
+	private TreeMap<Zona,HashSet<Animale>> zoo;
+	
+	/**
+	 * Metodo costruttore
+	 * @param nome
+	 * @param indirizzo
+	 * @param città
+	 */
+	public Zoo(String nome, String indirizzo, String città) {
+		this.setNome(nome);
+		this.setIndirizzo(indirizzo);
+		this.setCittà(città);
+		this.setAnimali(new TreeMap<>());
+	}
+
+	/**
+	 * Imposta il nome dello zoo
+	 * @param nome
+	 */
+	private void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * Imposta l'indirizzo dello zoo
+	 * @param indirizzo
+	 */
+	private void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	/**
+	 * Imposta la città dello zoo
+	 * @param città
+	 */
+	private void setCittà(String città) {
+		this.città = città;
+	}
+
+	/**
+	 * Imposta la TreeMap
+	 * @param animali
+	 */
+	private void setAnimali(TreeMap<Zona, HashSet<Animale>> animali) {
+		this.zoo = animali;
+	}
+	
+	/**
+	 * Restituisce la TreeMap
+	 * @return
+	 */
+	public TreeMap<Zona, HashSet<Animale>> getAnimali() {
+		return zoo;
+	}
+
+	/**
+	 * Rimuovi una zona dallo zoo
+	 * @param zona
+	 * @throws ZonaPresenteEccezione
+	 */
+	public void aggiungiZona(Zona zona) throws ZonaPresenteEccezione{
+		if (zoo.containsKey(zona)){
+			throw new ZonaPresenteEccezione();
+		} else {
+			zoo.put(zona, new HashSet<>());
+		}
+	}
+
+	/**
+	 * Rimuovi una zona dallo zoo
+	 * @param zona
+	 * @throws ZonaNonVuotaEccezione
+	 */
+	public void rimuoviZona(Zona zona) throws ZonaNonVuotaEccezione{
+		if (zoo.get(zona).isEmpty()){
+			zoo.remove(zona,zoo.get(zona));
+		} else {
+			throw new ZonaNonVuotaEccezione();
+		}
+	}
+	
+	/**
+	 * Aggiungi un animale allo zoo
+	 * @param zona
+	 * @param animale
+	 * @throws AnimalePresenteEccezione
+	 */
+	public void aggiungiAnimale(Zona zona, Animale animale) throws AnimalePresenteEccezione{
+		if (zoo.values().contains(animale)){
+			throw new AnimalePresenteEccezione();
+		} else {
+			zoo.get(zona).add(animale);
+		}
+	}
+	
+	/**
+	 * Restituisce tutti gli animali presenti nello zoo
+	 * @return
+	 */
+	public HashSet<HashSet<Animale>> animali(){
+		HashSet<HashSet<Animale>> lista = new HashSet<>(zoo.values());
+		return lista;
+	}
+	
+	/**
+	 * Rimuovi un animale dallo zoo
+	 * @param animale
+	 * @throws AnimaleNonPresenteEccezione
+	 */
+	public void rimuoviAnimale(Animale animale) throws AnimaleNonPresenteEccezione{
+		if (zoo.values().contains(animale)){
+			zoo.values().remove(animale);
+		} else {
+			throw new AnimaleNonPresenteEccezione();
+		}
+	}
+	
+	/**
+	 * Restituisce il numero di cani presenti nello zoo
+	 * @return 
+	 */
+	public int numeroCani(){
+		int numero = 0;
+		for (Zona z:zoo.keySet()){
+			for (Animale a: zoo.get(z)){
+				if (a instanceof Cane){
+					numero++;
+				}
+			}
+		}
+		return numero;
+	}
+	
+	/**
+	 * Conta i gatti presenti in una zona
+	 * @param animali
+	 * @return
+	 */
+	private int numeroGatti(HashSet<Animale> animali){
+		int numero = 0;
+		for (Animale a:animali){
+			if (a instanceof Gatto){
+				numero++;
+			}
+		}
+		return numero;
+	}
+	
+	/**
+	 * Restituisce la zona con più gatti
+	 * @return
+	 */
+	public Zona conPiùGatti(){
+		Zona zona=null;
+		int gatti = 0;
+		for (Zona z:zoo.keySet()){
+			if (numeroGatti(zoo.get(z))>gatti){
+				zona = z;
+				gatti = numeroGatti(zoo.get(z));
+			}
+		}
+		return zona;
+	}
+
+	@Override
+	/**
+	 * Implementa l'iteratore
+	 */
+	public Iterator<Zona> iterator() {
+		return zoo.keySet().iterator();
+	}	
+	
+}
